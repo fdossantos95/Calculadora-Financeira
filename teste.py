72755d3f3b68a93f1c127d945fa3130e09b92fac
fdossantos95 <fdossantos95@hotmail.com>
2020-12-17 20:54:54 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,13 +1,6 @@
 from PySimpleGUI import PySimpleGUI as sg 
 
 
-# tema
-sg.theme('Reddit')
-# layout
-layout = [
-    [sg.Text('Primeiro')],sg.input(key='primeiro')
-    [sg.Text('Segundo')],sg.Input(key='segundo')
-    [sg.Button('OK')]
-]
-# janela
-# ler os eventos
+# Layout
+# Janela
+# Ler eventos
