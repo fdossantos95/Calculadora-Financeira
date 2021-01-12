58c1965fc147050d12eb09de07adaa130d8ffcb1
fdossantos95 <fdossantos95@hotmail.com>
2021-01-12 17:55:37 -0300

    size da GUI

--- interface.py
+++ interface.py
@@ -7,25 +7,25 @@ sg.theme('Reddit')
 # layout
 layout = [
     [sg.Text('Modalidade'),sg.Text('FAST'),sg.Text('NORMAL')],
-    [sg.Text('MDR Débito',size=(1,1)),sg.Input(key='mdr_debito_fast'),sg.Input(key='mdr debito normal')],
-    [sg.Text('MDR Crédito'),sg.Input(key='mdr credito fast'),sg.Input(key='mdr_credito_normal')],
-    [sg.Text('MDR 2a6X'),sg.Input(key='mdr 2a6x fast'),sg.Input(key='mdr 2a6x normal')],
-    [sg.Text('MDR 7a12X'),sg.Input(key='mdr 7a12x fast'),sg.Input(key='mdr 7a12x normal')],
-    [sg.Text('RAV'),sg.Input(key='rav fast'),sg.Input(key='rav_normal')],
+    [sg.Text('MDR Débito'),sg.Input(key='mdr_debito_fast',size=(10,0)),sg.Input(key='mdr debito normal',size=(10,0))],
+    [sg.Text('MDR Crédito'),sg.Input(key='mdr credito fast',size=(10,0)),sg.Input(key='mdr_credito_normal',size=(10,0))],
+    [sg.Text('MDR 2a6X'),sg.Input(key='mdr 2a6x fast',size=(10,0)),sg.Input(key='mdr 2a6x normal',size=(10,0))],
+    [sg.Text('MDR 7a12X'),sg.Input(key='mdr 7a12x fast',size=(10,0)),sg.Input(key='mdr 7a12x normal',size=(10,0))],
+    [sg.Text('RAV'),sg.Input(key='rav fast',size=(10,0)),sg.Input(key='rav_normal',size=(10,0))],
     [sg.Text('Modalidade'),sg.Text('FAST'),sg.Text('NORMAL')],
-    [sg.Text('Débito'),sg.Input(key=(f'mdr_debito_fast')),     sg.Input(key='debito normal')],
-    [sg.Text('Crédito'),    sg.Input(key='credito fast'),    sg.Input(key='credito normal')],
-    [sg.Text('Parc. 2X'),   sg.Input(key='2x fast'),         sg.Input(key='2x normal')],
-    [sg.Text('Parc. 3X'),   sg.Input(key='3x fast'),         sg.Input(key='3x normal')],
-    [sg.Text('Parc. 4X'),   sg.Input(key='4x fast'),         sg.Input(key='4x normal')],
-    [sg.Text('Parc. 5X'),   sg.Input(key='5x fast'),         sg.Input(key='5x normal')],
-    [sg.Text('Parc. 6X'),   sg.Input(key='6x fast'),         sg.Input(key='6x normal')],
-    [sg.Text('Parc. 7x'),   sg.Input(key='7x fast'),         sg.Input(key='7x normal')],
-    [sg.Text('Parc. 8x'),   sg.Input(key='8x fast'),         sg.Input(key='8x normal')],
-    [sg.Text('Parc. 9x'),   sg.Input(key='9x fast'),         sg.Input(key='9x normal')],
-    [sg.Text('Parc. 10x'),  sg.Input(key='10x fast'),        sg.Input(key='10x normal')],
-    [sg.Text('Parc. 11x'),  sg.Input(key='11x fast'),        sg.Input(key='11x normal')],
-    [sg.Text('Parc. 12x'),  sg.Input(key='12x fast'),        sg.Input(key='12x normal')]
+    [sg.Text('Débito'),sg.Input(key='mdr_debito_fast',size=(10,0)),sg.Input(key='debito normal',size=(10,0))],
+    [sg.Text('Crédito'),sg.Input(key='credito fast',size=(10,0)),sg.Input(key='credito normal',size=(10,0))],
+    [sg.Text('Parc. 2X'),sg.Input(key='2x fast',size=(10,0)),sg.Input(key='2x normal',size=(10,0))],
+    [sg.Text('Parc. 3X'),sg.Input(key='3x fast',size=(10,0)),sg.Input(key='3x normal',size=(10,0))],
+    [sg.Text('Parc. 4X'),sg.Input(key='4x fast',size=(10,0)),sg.Input(key='4x normal',size=(10,0))],
+    [sg.Text('Parc. 5X'),sg.Input(key='5x fast',size=(10,0)),sg.Input(key='5x normal',size=(10,0))],
+    [sg.Text('Parc. 6X'),sg.Input(key='6x fast',size=(10,0)),sg.Input(key='6x normal',size=(10,0))],
+    [sg.Text('Parc. 7x'),sg.Input(key='7x fast',size=(10,0)),sg.Input(key='7x normal',size=(10,0))],
+    [sg.Text('Parc. 8x'),sg.Input(key='8x fast',size=(10,0)),sg.Input(key='8x normal',size=(10,0))],
+    [sg.Text('Parc. 9x'),sg.Input(key='9x fast',size=(10,0)),sg.Input(key='9x normal',size=(10,0))],
+    [sg.Text('Parc. 10x'),sg.Input(key='10x fast',size=(10,0)),sg.Input(key='10x normal',size=(10,0))],
+    [sg.Text('Parc. 11x'),sg.Input(key='11x fast',size=(10,0)),sg.Input(key='11x normal',size=(10,0))],
+    [sg.Text('Parc. 12x'),sg.Input(key='12x fast',size=(10,0)),sg.Input(key='12x normal',size=(10,0))]
 ]
 # window
 janela = sg.Window('CALCULADORA FINANCEIRA', layout)
