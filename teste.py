05c665e608c8db6505383dd96b8ba42982282f30
fdossantos95 <fdossantos95@hotmail.com>
2021-01-18 22:04:31 -0300

    .

--- teste.py
+++ teste.py
@@ -1,2 +1,19 @@
-n=int(input('->'))
-print(n)
+import PySimpleGUI as sg
+
+sg.theme('DarkAmber')
+
+layout = [ [sg.Text('Operação de soma')],
+           [sg.Text('Digite o primeiro número:',size=(25,0)), sg.InputText('0',size=(10,0))],
+           [sg.Text('Digite o segundo número:',size=(25,0)), sg.InputText('0',size=(10,0))],
+           [sg.Button('Ok'), sg.Input(size=(10,0))] ]
+
+window = sg.Window('Window Title', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+
+print('You entered ', values[0])
+window.close()
+
